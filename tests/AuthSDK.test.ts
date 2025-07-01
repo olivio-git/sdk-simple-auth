3c6fbfb041171e7c3f17c9175572bd3f7825ad1d
olivio-dev <tj.olivio.subelza.c@upds.net.bo>
2025-06-30 23:40:50 -0400

    feature: update default test

--- tests/AuthSDK.test.ts
+++ tests/AuthSDK.test.ts
@@ -20,9 +20,9 @@ describe('AuthSDK', () => {
 
   test('should login successfully', async () => {
     const mockResponse = {
-      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODYzMDYyZjBkODZhNGFjMDgxMWZhMmUiLCJpYXQiOjE3NTEzMzgyMTEsImV4cCI6MTc1MTMzOTExMX0.z6ng9aYz9Brlr8YUcI4QzyO-g2oNYBgr59BIxpYv1bo',
-      refresh_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODYzMDYyZjBkODZhNGFjMDgxMWZhMmUiLCJpYXQiOjE3NTEzMzgyMTEsImV4cCI6MTc1MTk0MzAxMX0.rS8EYVOrzC-poRDohvBmtv7wbrtIWkOshnESoiAG16w',
-      user: { id: '6863062f0d86a4ac0811fa2e', email: 'subelzaolivitocabezas@gmail.com' },
+      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+      refresh_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+      user: { id: '6863062f0d86a4ac0811fa2e', email: 'user@example.com' },
     };
 
     (fetch as jest.Mock).mockResolvedValueOnce({
@@ -31,7 +31,7 @@ describe('AuthSDK', () => {
     });
 
     const user = await authSDK.login({
-      email: 'subelzaolivitocabezas@gmail.com',
+      email: 'user@example.com',
       password: 'olivio12',
     });
 
@@ -39,6 +39,5 @@ describe('AuthSDK', () => {
     expect(authSDK.isAuthenticated()).toBe(true);
   });
 
-  // Más tests... Aquí puedes agregar más pruebas para otros métodos como register, logout, etc.
 });
  
